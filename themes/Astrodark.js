9d68e154219711bf981c08f545ca11f365e99741
Junior Sundar <juniorsundar@gmail.com>
2025-08-02 15:12:36 +0400

    feat: add Astrodark

---
+++ themes/Astrodark.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides', ["#111317", "#ff838b", "#87c05f", "#dfab25", "#5eb7ff", "#dd97f1", "#4ac2b8", "#9b9fa9", "#34363a", "#ffa6ae", "#aae382", "#ffce48", "#81daff", "#ffbaff", "#6de5db", "#d0d3de"]);
+t.prefs_.set('foreground-color', "#adb0bb");
+t.prefs_.set('background-color', "#1a1d23");
+t.prefs_.set('cursor-color', "#adb0bb");
